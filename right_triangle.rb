ac6114a62ad089408aa8468c48a7268c636b4f48
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-03-01 13:00:34 +0300

    Lesson 1 v2

--- right_triangle.rb
+++ right_triangle.rb
@@ -7,26 +7,21 @@ second_side = gets.chomp.to_i
 print "Введите длину 3 стороны треугольника: "
 third_side = gets.chomp.to_i
 
-isosceles = (first_side == second_side || first_side == third_side || second_side == third_side)
+a, b, hypo = [first_side, second_side, third_side].sort
+isosceles = (hypo == a || hypo == b || a == b)
+right_triangle = hypo**2 == a**2 + b**2 
 
-if first_side > 0 && second_side > 0 && third_side > 0
-	if first_side == second_side && first_side == third_side
-		puts "Треугольник равносторонний!"
-	elsif first_side > second_side && first_side > third_side	
-		right_triangle = first_side**2 == second_side**2 + third_side**2
-		puts "Треугольник прямоугольный? #{right_triangle}"
-		puts "Треугольник равнобедренный? #{isosceles}" 
-	elsif second_side > first_side && second_side > third_side
-		right_triangle = second_side**2 == first_side**2 + third_side**2
-		puts "Треугольник прямоугольный? #{right_triangle}"
-		puts "Треугольник равнобедренный? #{isosceles}"
-	elsif third_side > first_side && third_side > second_side 
-		right_triangle = third_side**2 == first_side**2 + second_side**2
-		puts "Треугольник прямоугольный? #{right_triangle}"
-		puts "Треугольник равнобедренный? #{isosceles}"	
-	else
-		puts "Треугольник обычный!" 
-	end
+if hypo > 0 && a > 0 && b > 0
+    if hypo == a && hypo == b
+        puts "Треугольник равносторонний!"
+    elsif right_triangle
+        puts "Треугольник прямоугольный? #{right_triangle}"
+        puts "Треугольник равнобедренный? #{isosceles}"
+    elsif isosceles
+       puts "Треугольник равнобедренный!"
+    else
+        puts "Треугольник обычный!" 
+    end
 else 
-	puts "Введите корректные данные!"
+    puts "Введите корректные данные!"
 end
